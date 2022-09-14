b85be79376bbabd8a738249fa7946b9430721491
mjkang0987 <snowdrop1202@gmail.com>
2022-09-14 16:05:34 +0900

    feat: React + Next.js 설정

---
+++ client/pages/index.tsx
@@ -0,0 +1,32 @@
+import type {NextPage} from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>RESERVATION</title>
+                <meta name="description" content="meta content description"/>
+                <meta name="keywords" content="meta keywords"/>
+                <meta name="author" content="MJ Kang"/>
+                <meta name="og:site_name" content="RESERVATION"/>
+                <meta name="og:title" content="service title"/>
+                <meta name="og:description" content="service description"/>
+                <meta name="og:type" content="website" />
+                <meta name="og:url" content="service url"/>
+                <meta name="og:image" content="image url"/>
+                <meta name="twitter:title" content="service title"/>
+                <meta name="twitter:description" content="service description"/>
+                <meta name="twitter:card" content="summary" />
+                <meta name="og:url" content="service url"/>
+                <meta name="twitter:image" content="image url"/>
+            </Head>
+            <main>
+                <h1>service reservation</h1>
+                
+            </main>
+        </>
+    );
+};
+
+export default Home;
