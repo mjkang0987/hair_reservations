1bd83d3567cf54ce428900a5cca942be88cb544e
mjkang0987 <snowdrop1202@gmail.com>
2022-09-14 16:08:14 +0900

    feat: 글로벌 컴포넌트 추가

---
+++ client/styles/global-style.ts
@@ -0,0 +1,27 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html,
+    body {
+        padding: 0;
+        margin: 0;
+    }
+    
+    h1,
+    h2,
+    div,
+    p,
+    a,
+    button,
+    aside,
+    main,
+    header,
+    footer,
+    section,
+    article {
+        margin: 0;
+        padding: 0;
+    }
+    
+    
+`;
