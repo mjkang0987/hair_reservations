ef5cbfee4e470e116ddf021737cb726f82f62f44
mjkang0987 <snowdrop1202@gmail.com>
2022-09-15 16:33:22 +0900

    chore: 전역 스타일 프리티어 적용

--- client/styles/globalStyle.ts
+++ client/styles/globalStyle.ts
@@ -1,25 +1,39 @@
-import {createGlobalStyle} from "styled-components";
+import {createGlobalStyle} from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-    html,
-    body {
-        padding: 0;
-        margin: 0;
-    }
-    
-    h1,
-    h2,
-    div,
-    p,
-    a,
-    button,
-    aside,
-    main,
-    header,
-    footer,
-    section,
-    article {
-        margin: 0;
-        padding: 0;
-    }
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+  }
+
+  h1,
+  h2,
+  div,
+  p,
+  a,
+  button,
+  aside,
+  main,
+  header,
+  footer,
+  section,
+  article {
+    margin: 0;
+    padding: 0;
+  }
+
+  main {
+    display: grid;
+    height: 100%;
+  }
+  
+  button,
+  input {
+    background-color: transparent;
+    border: 1px solid #ccc;
+    padding: 5px 10px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, .1);
+  }
 `;
