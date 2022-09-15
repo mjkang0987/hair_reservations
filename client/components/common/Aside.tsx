8940e82953527f2a907670036e5296d29a035b53
mjkang0987 <snowdrop1202@gmail.com>
2022-09-15 16:31:25 +0900

    chore: 공통 컴포넌트명 변경(Aside, Footer, Header)

--- client/components/common/Aside.tsx
+++ client/components/common/Aside.tsx
@@ -1,5 +1,37 @@
-export const Aside = () => {
+import styled from 'styled-components';
+
+import {ASIDE as asides} from '../../utils/constants/constants';
+
+export const AsideComponent = () => {
     return (
-        <aside>aside</aside>
+        <Aside>
+            {Object.keys(asides).map((a) =>
+                <Button key={asides[a].id} type="button">
+                    <ButtonText buttonType={asides[a].title === '일정추가'}>
+                        {asides[a].title}
+                    </ButtonText>
+                </Button>
+            )}
+        </Aside>
     );
 };
+
+const Aside = styled.aside`
+  padding: 0 10px;
+`;
+
+const Button = styled.button`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+
+  + button {
+    margin-top: 10px;
+  }
+`;
+
+const ButtonText = styled.span<{ buttonType: boolean }>`
+  display: flex;
+  padding-left: ${props => props.buttonType ? `20px` : 0}
+`;
