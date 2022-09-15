ee19402e646cde8b7409845b4b19dfc4bc255e18
mjkang0987 <snowdrop1202@gmail.com>
2022-09-15 10:26:32 +0900

    chore: 전역 스타일 파일명 카멜케이스로 변경

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type {AppProps} from 'next/app';
-import {GlobalStyle} from '../styles/global-style';
+import {GlobalStyle} from '../styles/globalStyle';
 
 function App({Component, pageProps}: AppProps) {
     return (
