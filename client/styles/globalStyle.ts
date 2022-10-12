e53a662f14161a4035599e5d9cb1d6d7d2806382
mjkang0987 <snowdrop1202@gmail.com>
2022-10-12 14:15:19 +0900

    chore: button, input, a tag 공통 스타일 분기

--- client/styles/globalStyle.ts
+++ client/styles/globalStyle.ts
@@ -30,17 +30,28 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
 
-  main {
-    display: grid;
-    height: 100%;
+  button,
+  input,
+  a {
+    font-size: var(--defaultFont);
+    color: var(--defaultBlack);
   }
-  
+
   button,
   input {
-    background-color: transparent;
+    height: 40px;
     border: 1px solid #ccc;
-    padding: 5px 10px;
+    background-color: var(--defaultWhite);
     border-radius: 5px;
     box-shadow: 0 0 10px 0 rgba(0, 0, 0, .1);
+  }
+
+  button,
+  a {
+    cursor: pointer;
+
+    &:active {
+      opacity: .6;
+    }
   }
 `;
