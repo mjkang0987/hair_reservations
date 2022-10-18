88245702b3c4a40aaa2ac2cc3a82398fec26ada6
mjkang0987 <snowdrop1202@gmail.com>
2022-10-18 15:02:16 +0900

    feat: `Icon` component 분리

---
+++ client/components/common/Icons.tsx
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import React from 'react';
+
+interface Props {
+    iconType: string
+}
+
+const StyledIcon = styled.span <Props>`
+  flex-shrink: 0;
+  display: inline-flex;
+  position: relative;
+  width: 40px;
+  ${props => props.iconType === 'search' && `
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+    }
+    
+    &::before {
+      top: 10px;
+      right: 14px;
+      width: 14px;
+      height: 14px;
+      border: solid 1px var(--defaultBlack);
+      border-radius: 100%;
+    }
+    
+    &::after {
+      top: 26px;
+      right: 12px;
+      width: 6px;
+      height: 1px;
+      background-color: var(--defaultBlack);
+      transform: rotate(45deg);
+    }
+  `}
+
+  ${props => props.iconType === 'plus' && `
+    &::after,
+    &::before {
+      content: "";
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      width: 16px;
+      height: 2px;
+      background-color: var(--defaultBlack);
+      border-radius: 2px;
+      pointer-events: none;
+    }
+    
+    &::before {
+      transform: translate(-50%, -50%);
+    }
+    
+    &:after {
+      transform: translate(-50%, -50%) rotate(90deg);
+    }
+  `};
+`;
+
+export const Icon: React.FC <Props> = ({iconType}) => {
+    return <StyledIcon iconType={iconType}/>;
+};
