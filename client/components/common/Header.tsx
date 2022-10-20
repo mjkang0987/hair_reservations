69f3d9120a0e9a262323b8f4f59563e1f5e00165
mjkang0987 <snowdrop1202@gmail.com>
2022-10-20 17:34:59 +0900

    feat: `Header` 스타일 추가

--- client/components/common/Header.tsx
+++ client/components/common/Header.tsx
@@ -1,5 +1,33 @@
+import {Icon} from './Icons';
+import {ButtonText} from './ButtonText';
+import styled from 'styled-components';
+
 export const HeaderComponent = () => {
     return (
-        <header>header</header>
+        <Header>
+            <Button type="button">
+                <Icon iconType="hamburger"/>
+                <ButtonText a11y={true}>메뉴 닫기</ButtonText>
+            </Button>
+        </Header>
     );
 };
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  padding: 0 5px;
+`;
+
+const Button = styled.button`
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+  background-color: #fff;
+  border: none;
+  
+  &:hover {
+    background-color: rgba(0, 0, 0, .1);
+  }
+`;
