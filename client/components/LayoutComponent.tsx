dd15cfd1b04d2596bf5b95c266c3ff6ba0841845
mjkang0987 <snowdrop1202@gmail.com>
2022-10-20 17:37:17 +0900

    feat: loading icon 적용

--- client/components/LayoutComponent.tsx
+++ client/components/LayoutComponent.tsx
@@ -1,24 +1,40 @@
-import {ReactNode} from 'react';
+import {ReactNode, useState} from 'react';
 
 import styled from 'styled-components';
+
+import {useIsomorphicEffect} from '../hooks/useIsomorphicEffect';
 
 import {HeaderComponent} from './common/Header';
 import {AsideComponent} from './common/Aside';
 import {FooterComponent} from './common/Footer';
+import {Icon} from './common/Icons';
 
 type LayoutProps = {
     children: ReactNode
 };
 
 export default function LayoutComponent({children}: LayoutProps) {
+    const [loading, setLoading] = useState(false);
+
+    const isomorphicEffect = useIsomorphicEffect();
+
+    isomorphicEffect(() => {
+        console.log(loading)
+        setLoading(true);
+        console.log(loading)
+    }, []);
+
     return (
         <>
-            <HeaderComponent/>
-            <Main>
-                <AsideComponent/>
-                {children}
-            </Main>
-            <FooterComponent/>
+            {!loading && <Icon iconType="loading"/>}
+            {loading && <>
+                <HeaderComponent/>
+                <Main>
+                    <AsideComponent/>
+                    {children}
+                </Main>
+                <FooterComponent/>
+            </>}
         </>
     );
 }
