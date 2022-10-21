14f2c1464b43bc1a3040676fcf57133ed1b2711d
mjkang0987 <snowdrop1202@gmail.com>
2022-10-21 17:15:04 +0900

    refactor: Recoil atoms ts로 변경

--- client/recoil/atoms.js
+++
@@ -1,9 +0,0 @@
-import {atom} from 'recoil';
-
-export const asideState = atom({
-    key: 'asideState',
-    default: {
-        isVisible: true,
-        isTransitionEnd: true
-    }
-});
---
+++ client/recoil/atoms.ts
@@ -0,0 +1,14 @@
+import {atom} from 'recoil';
+
+interface AsideType {
+    isVisible: boolean;
+    isTransitionEnd: boolean;
+}
+
+export const asideState = atom<AsideType>({
+    key    : 'asideState',
+    default: {
+        isVisible      : true,
+        isTransitionEnd: true
+    }
+});
