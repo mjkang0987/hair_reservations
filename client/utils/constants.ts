e2d82767f2dc7b171cdac0a5dd87570fb888b311
mjkang0987 <snowdrop1202@gmail.com>
2022-10-24 17:37:13 +0900

    feat: 요일 상수 추가

--- client/utils/constants.ts
+++ client/utils/constants.ts
@@ -27,6 +27,55 @@ const ASIDE: AsideType = {
     },
 };
 
+interface DaysElementType {
+    id: number;
+    ko: string;
+    en: string;
+}
+
+interface DaysType {
+    [key: string]: DaysElementType;
+}
+
+const DAYS: DaysType = {
+    SUN: {
+        id: 1,
+        ko: '일',
+        en: 'SUNDAY'
+    },
+    MON: {
+        id: 2,
+        ko: '월',
+        en: 'MONDAY'
+    },
+    TUE: {
+        id: 3,
+        ko: '화',
+        en: 'TUESDAY'
+    },
+    WED: {
+        id: 4,
+        ko: '수',
+        en: 'WEDNESDAY'
+    },
+    THUR: {
+        id: 5,
+        ko: '목',
+        en: 'THURSDAY'
+    },
+    FRI: {
+        id: 6,
+        ko: '금',
+        en: 'FRIDAY'
+    },
+    SAT: {
+        id: 7,
+        ko: '토',
+        en: 'SATURDAY'
+    }
+};
+
 export {
-    ASIDE
+    ASIDE,
+    DAYS
 };
