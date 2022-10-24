38b4a7e86f85fa1ac827c0f28db6feee9ea3c905
mjkang0987 <snowdrop1202@gmail.com>
2022-10-24 16:51:42 +0900

    feat: 상단 노출 날짜 상태 추가

--- client/components/LayoutComponent.tsx
+++ client/components/LayoutComponent.tsx
@@ -2,7 +2,11 @@ import {ReactNode, useState} from 'react';
 
 import styled from 'styled-components';
 
+import {useRecoilState, useSetRecoilState} from 'recoil';
+
 import {useIsomorphicEffect} from '../hooks/useIsomorphicEffect';
+
+import {currentDate, todayDate} from '../recoil/atoms';
 
 import {HeaderComponent} from './common/Header';
 import {AsideComponent} from './common/Aside';
@@ -16,11 +20,29 @@ type LayoutProps = {
 export default function LayoutComponent({children}: LayoutProps) {
     const [loading, setLoading] = useState(false);
 
+    const [today, setToday] = useRecoilState(todayDate);
+    const setCurrentDate = useSetRecoilState(currentDate);
+
     const isomorphicEffect = useIsomorphicEffect();
 
     isomorphicEffect(() => {
+        const getToday = new Date();
+
+        setToday({
+            year: getToday.getFullYear(),
+            month: getToday.getMonth(),
+            date: getToday.getDate(),
+            day: getToday.getDay(),
+        });
+
         setLoading(true);
     }, []);
+
+    isomorphicEffect(() => {
+        setCurrentDate({
+            ...today
+        });
+    }, [today])
 
     return (
         <>
--- client/recoil/atoms.ts
+++ client/recoil/atoms.ts
@@ -1,5 +1,32 @@
 import {atom} from 'recoil';
 
+interface TodayDateType {
+    year: null | number;
+    month: null | number;
+    date: null | number;
+    day: null | number;
+}
+
+export const todayDate = atom<TodayDateType>({
+    key: 'todayDate',
+    default: {
+        year: null,
+        month: null,
+        date: null,
+        day: null
+    }
+});
+
+export const currentDate = atom<TodayDateType>({
+    key: 'currentDate',
+    default: {
+        year: null,
+        month: null,
+        date: null,
+        day: null
+    }
+});
+
 interface AsideType {
     isVisible: boolean;
     isTransitionEnd: boolean;
