7c155007f15d742b47891e27ed22d41a238f46cf
mjkang0987 <snowdrop1202@gmail.com>
2022-11-15 16:36:20 +0900

    feat: `next.config.js` - path rewrites 추가

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify      : true
+};
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig,
+    async rewrites() {
+        const asides = ['day', 'three', 'week', 'month', 'year'];
+        let types = [];
+
+        asides.map((aside) => {
+            const type = {
+                source     : `/${aside.toLowerCase()}/:path*`,
+                destination: '/'
+            };
+
+            types = [...types, type];
+        });
+
+        return [
+            ...types
+        ];
+    }
+};
