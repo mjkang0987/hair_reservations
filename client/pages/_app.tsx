8c97aeed1c63e4c9b2afafbd8f3f29222dcdb173
mjkang0987 <snowdrop1202@gmail.com>
2022-11-15 16:39:50 +0900

    feat: title 추가

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+
+import Head from 'next/head';
 import type {AppProps} from 'next/app';
 
 import {RecoilRoot} from 'recoil';
@@ -9,6 +12,9 @@ import LayoutComponent from '../components/LayoutComponent';
 function App({Component, pageProps}: AppProps) {
     return (
         <RecoilRoot>
+            <Head>
+                <title>RESERVATION</title>
+            </Head>
             <LayoutComponent>
                 <GlobalStyle/>
                 <Component {...pageProps} />
