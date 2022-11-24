be3f478a453a415140d432ae3d59b3bae4b45b0f
mjkang0987 <snowdrop1202@gmail.com>
2022-11-24 17:48:54 +0900

    feat: `DaysComponent` Props `type` 추가

--- client/components/calendar/Calendar.tsx
+++ client/components/calendar/Calendar.tsx
@@ -4,13 +4,13 @@ import {viewState} from '../../recoil/atoms';
 import {DaysComponent} from './Days';
 import {DatesComponent} from './Dates';
 
-export const Calendar = ()  => {
+export const CalendarComponent = ()  => {
     const view = useRecoilValue(viewState);
     const {type} = view;
 
     return (
         <>
-            {(type === 'month' || type === 'week' || type === 'three') && <DaysComponent/>}
+            {(type === 'month' || type === 'week' || type === 'three') && <DaysComponent type={type}/>}
             <DatesComponent/>
         </>
     );
