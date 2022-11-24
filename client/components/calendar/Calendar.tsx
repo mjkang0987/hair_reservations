553113f0eefa5ee2f1d8781f7f7ff110705ddfa1
mjkang0987 <snowdrop1202@gmail.com>
2022-11-24 13:21:33 +0900

    feat: 요일 노출 분기

--- client/components/calendar/Calendar.tsx
+++ client/components/calendar/Calendar.tsx
@@ -1,10 +1,16 @@
+import {useRecoilValue} from 'recoil';
+import {viewState} from '../../recoil/atoms';
+
 import {DaysComponent} from './Days';
 import {DatesComponent} from './Dates';
 
 export const Calendar = ()  => {
+    const view = useRecoilValue(viewState);
+    const {type} = view;
+
     return (
         <>
-            <DaysComponent/>
+            {(type === 'month' || type === 'week' || type === 'three') && <DaysComponent/>}
             <DatesComponent/>
         </>
     );
