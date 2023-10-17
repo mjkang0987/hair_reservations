2dd5ad73befd7ff4faee4820dc8c7b7e674ede10
mjkang0987 <snowdrop1202@gmail.com>
2023-10-17 14:09:26 +0900

    refactor: `ViewType` 상수 import

--- client/components/calendar/WeekWrap.tsx
+++ client/components/calendar/WeekWrap.tsx
@@ -56,7 +56,6 @@ export const WeekWrapComponent = ({
                     });
                 }}>{w}</Num>
             </Week>)}
-            {(type === 'week' && (weekLastDay < 6)) && new Array(6 - weekLastDay).fill(null).map((_, index) => <Week key={`next_${index}`}>
             {(type === ViewType.Week && (weekLastDay < 6)) && new Array(6 - weekLastDay).fill(null).map((_, index) => <Week key={`next_${index}`}>
                 <Num onClick={() => {
                     setDate({
@@ -65,7 +64,6 @@ export const WeekWrapComponent = ({
                     });
                 }}>{index + 1}</Num>
             </Week>)}
-            {(type === 'three' && (monthLastNumber - date < 2)) && new Array((monthLastNumber - date === 0 ? 2 : monthLastNumber - date)).fill(null).map((_, index) => <Week key={`next_${index}`}>
             {(type === ViewType.Three && (monthLastNumber - date < 2)) && new Array((monthLastNumber - date === 0 ? 2 : monthLastNumber - date)).fill(null).map((_, index) => <Week key={`next_${index}`}>
                 <Num onClick={() => {
                     setDate({
