1b2f93a12ca3939a0833769765d4d789078fd0af
mjkang0987 <snowdrop1202@gmail.com>
2023-11-02 17:26:35 +0900

    refactor: 날짜 변경 최초 `LayoutComponent.tsx`에서 진행되도록 변경
    header, asdie 등 공통으로 사용되는 영역이 달력 외에도 노출되어야하므로 변경

--- client/components/LayoutComponent.tsx
+++ client/components/LayoutComponent.tsx
@@ -1,4 +1,5 @@
 import React, {
+    useEffect,
     useState
 } from 'react';
 
@@ -7,14 +8,18 @@ import {useRouter} from 'next/router';
 import styled from 'styled-components';
 
 import {
-    useRecoilState
+    useRecoilState,
+    useRecoilValue,
+    useSetRecoilState
 } from 'recoil';
 
 import {
     asideState,
+    routerState,
+    targetState,
     targetStateState,
     todayState,
-    viewState
+    viewState,
 } from '../recoil/atoms';
 
 import {useIsomorphicEffect} from '../hooks/useIsomorphicEffect';
@@ -24,6 +29,8 @@ import {
 } from '../hooks/useCloseModal';
 
 import {
+    handleOnload,
+    isCalendar,
     NodeType,
     setRouter,
     ViewType
@@ -33,20 +40,21 @@ import {HeaderComponent} from './common/Header';
 import {AsideComponent} from './common/Aside';
 import {FooterComponent} from './common/Footer';
 import {Icon} from './common/Icons';
+import {ButtonText} from './common/ButtonText';
 
 export default function LayoutComponent({children}: NodeType) {
     const router = useRouter();
 
     const [loading, setLoading] = useState(false);
-    const [today, setToday] = useRecoilState(todayState);
     const [aside, setAside] = useRecoilState(asideState);
-    const [view, setView] = useRecoilState(viewState);
-    const [curr, setCurr] = useRecoilState(targetStateState);
+    const setToday = useSetRecoilState(todayState);
+    const [routers, setRouters] = useRecoilState(routerState);
+    const currValue = useRecoilValue(targetState);
+    const setCurr = useSetRecoilState(targetStateState);
+    const setView = useSetRecoilState(viewState);
+
 
     const isomorphicEffect = useIsomorphicEffect();
-
-    const arrayPath = router.asPath.split('/');
-    const isInitPath = arrayPath.length === 2;
 
     const initDate: Date = new Date();
 
@@ -68,35 +76,53 @@ export default function LayoutComponent({children}: NodeType) {
         });
     };
 
+    const array = router.asPath.split('/');
+    const isRootPath = array.join('').length === 0;
+    const isCalendarPath = isCalendar(array);
+
+    const currDate = !isCalendarPath || isRootPath ? initDate : new Date(Number(array[2]), Number(array[3]) - 1, Number(array[4]) || 1);
+
+    handleOnload({
+        setRouters
+    });
+
     isomorphicEffect(() => {
         setLoading(true);
         setToday(initDate);
+        setCurr(currDate);
 
-        setCurr(isInitPath ? initDate : new Date(Number(arrayPath[2]), Number(arrayPath[3]) - 1, (Number(arrayPath[4]) || 1)));
-        setView({
-            type: isInitPath
-                  ? ViewType.Week
-                  : arrayPath[1]
+        setRouters({
+            arrayRouter: router.asPath.split('/'),
+            isRootPath,
+            isCalendarPath
         });
 
         setRouter({
-            type: isInitPath
-                  ? ViewType.Week
-                  : arrayPath[1],
-            year: isInitPath ? initDate.getFullYear() : Number(arrayPath[2]),
-            month: isInitPath ? initDate.getMonth() + 1 : Number(arrayPath[3]),
-            date: isInitPath ? initDate.getDate() : (Number(arrayPath[4]) || 1),
+            type : isRootPath ? ViewType.Week : array[1],
+            year : currDate.getFullYear(),
+            month: currDate.getMonth() + 1,
+            date : currDate.getDate(),
             router
         });
     }, []);
 
-    return (
-        <StyledWrapper onClick={closeModal}>
+    useEffect(() => {
+        setView({
+            type: isRootPath || !isCalendarPath ? ViewType.Week : array[1]
+        });
+    }, [routers, setRouters]);
+
+    return (<StyledWrapper onClick={closeModal}>
             {!loading && <Icon iconType="loading"/>}
-            {(loading && today) && <>
-                <HeaderComponent/>
+            <HeaderComponent/>
+            {currValue.full !== null && <>
                 <StyledMain>
-                    {curr && <AsideComponent/>}
+                    <StyledButton type="button"
+                                  isVisible={aside.isVisible}>
+                        <Icon iconType="plus"/>
+                        {aside.isVisible && <ButtonText a11y={false}>일정추가</ButtonText>}
+                    </StyledButton>
+                    <AsideComponent/>
                     {children}
                 </StyledMain>
                 <FooterComponent/>
@@ -105,7 +131,7 @@ export default function LayoutComponent({children}: NodeType) {
     );
 }
 
-const StyledWrapper = styled.div`
+const StyledWrapper = styled.div<{ onClick?: Function }>`
   display: flex;
   flex-direction: column;
   height: 100%;
@@ -117,4 +143,37 @@ const StyledMain = styled.main`
   display: flex;
   height: 100%;
   position: relative;
+`;
+
+const StyledButton = styled.button <{ isVisible: boolean }>`
+  display: inline-flex;
+  position: absolute;
+  top: 10px;
+  left: 15px;
+  align-items: center;
+  justify-content: center;
+  width: ${props => props.isVisible
+                    ? '189px'
+                    : 'auto'};
+  max-width: calc(80% - 30px);
+  height: 35px;
+  border: 1px solid #ccc;
+  background-color: ${props => props.isVisible
+                               ? 'var(--white-color)'
+                               : 'rgb(255 255 255 / .6)'};
+  border-radius: ${props => props.isVisible
+                            ? '5px'
+                            : '20px'};
+  box-shadow: ${props => props.isVisible
+                         ? '0 0 10px 0 rgba(0, 0, 0, .1)'
+                         : '0 0 10px 0 rgba(0, 0, 0, .2)'};
+  font-size: var(--small-font);
+  z-index: 3;
+  transition: box-shadow .1s ease-in-out;
+
+  &:hover {
+    ${props => !props.isVisible && `
+      box-shadow:  0 0 15px 0 rgba(0, 0, 0, .4);
+    `}
+  }
 `;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import type {
+    NextPage
+} from 'next';
 
-import type {NextPage, InferGetStaticPropsType, GetStaticProps} from 'next';
 import Head from 'next/head';
 
-import {useRecoilValue} from 'recoil';
+import {
+    useRecoilValue,
+} from 'recoil';
 
 import styled from 'styled-components';
 
 import {
     asideState,
     targetStateState,
-    viewState
 } from '../recoil/atoms';
 
-import {Icon} from '../components/common/Icons';
-import {ButtonText} from '../components/common/ButtonText';
 import {CalendarComponent} from '../components/calendar/CalendarWrap';
 
 interface Props {
@@ -26,22 +26,15 @@ const Home: NextPage = ({
     reservations
 }: any) => {
     const aside = useRecoilValue(asideState);
-    const target = useRecoilValue(targetStateState);
-
-    console.log(reservations)
+    const curr = useRecoilValue(targetStateState);
 
     return (<>
             <Head>
                 <title>RESERVATION</title>
             </Head>
-            <StyledButton type="button"
-                          isVisible={aside.isVisible}>
-                <Icon iconType="plus"/>
-                {aside.isVisible && <ButtonText a11y={false}>일정추가</ButtonText>}
-            </StyledButton>
             <StyledSection isVisible={aside.isVisible}
                            isTransitionEnd={aside.isTransitionEnd}>
-                {target && <CalendarComponent/>}
+                {curr && <CalendarComponent/>}
             </StyledSection>
         </>
     );
@@ -64,13 +57,21 @@ const StyledButton = styled.button <{ isVisible: boolean }>`
   left: 15px;
   align-items: center;
   justify-content: center;
-  width: ${props => props.isVisible ? '189px' : 'auto'};
+  width: ${props => props.isVisible
+                    ? '189px'
+                    : 'auto'};
   max-width: calc(80% - 30px);
   height: 35px;
   border: 1px solid #ccc;
-  background-color: ${props => props.isVisible ? 'var(--white-color)' : 'rgb(255 255 255 / .6)'};
-  border-radius: ${props => props.isVisible ? '5px' : '20px'};
-  box-shadow: ${props => props.isVisible ? '0 0 10px 0 rgba(0, 0, 0, .1)' : '0 0 10px 0 rgba(0, 0, 0, .2)'};
+  background-color: ${props => props.isVisible
+                               ? 'var(--white-color)'
+                               : 'rgb(255 255 255 / .6)'};
+  border-radius: ${props => props.isVisible
+                            ? '5px'
+                            : '20px'};
+  box-shadow: ${props => props.isVisible
+                         ? '0 0 10px 0 rgba(0, 0, 0, .1)'
+                         : '0 0 10px 0 rgba(0, 0, 0, .2)'};
   font-size: var(--small-font);
   z-index: 3;
   transition: box-shadow .1s ease-in-out;
@@ -86,11 +87,9 @@ export const getStaticProps = (async () => {
     const res = await fetch('http://localhost:3000/api/hello');
     const reservations = await res.json() || {};
 
-    console.log(reservations)
-
     return {
         props: {
-            reservations
+            reservations,
         }
     };
 });
