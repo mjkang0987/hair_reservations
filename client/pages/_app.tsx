e445b2c5c28a85e2dbb2363b9ae7ec2edad33b9e
mjkang0987 <snowdrop1202@gmail.com>
2023-11-08 09:41:42 +0900

    chore: indent 변경

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -19,7 +19,7 @@ function App({Component, pageProps}: AppProps) {
             <Head>
                 <title>RESERVATION</title>
             </Head>
-                <GlobalStyle/>
+            <GlobalStyle/>
             <LayoutComponent>
                 <Component {...pageProps} />
             </LayoutComponent>
