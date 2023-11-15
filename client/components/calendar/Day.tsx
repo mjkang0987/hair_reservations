9284a17e70906fe46d5034a457fc326694c3a4e1
mjkang0987 <snowdrop1202@gmail.com>
2023-11-15 13:30:48 +0900

    feat: 일별 보기 내 `Reservation` 추가

--- client/components/calendar/Day.tsx
+++ client/components/calendar/Day.tsx
@@ -1,23 +1,25 @@
 import {
-    useRecoilState,
     useRecoilValue,
 } from 'recoil';
 
 import {
+    currReservationsState,
     targetStateState,
-    todayState,
-    viewState
+    todayState
 } from '../../recoil/atoms';
 
 import {
+    filterReservations,
     isTodayValue
 } from '../../utils/utils';
 
 import {TimelineComponent} from './Timeline';
+import {ReservationComponent} from '../reservation/Reservation';
 
 export const DayComponent = () => {
     const today = useRecoilValue(todayState);
     const curr = useRecoilValue(targetStateState);
+    const currReservations = useRecoilValue(currReservationsState);
 
     const {
         fullYear,
@@ -25,9 +27,18 @@ export const DayComponent = () => {
         date,
     } = curr;
 
-    return (<TimelineComponent fullYear={fullYear}
+    return (<>
+            <TimelineComponent fullYear={fullYear}
                                month={month}
                                date={date}
-                               isToday={isTodayValue(today, fullYear, month, date)}/>
+                               isToday={isTodayValue(today, fullYear, month, date)}>
+            <ReservationComponent items={filterReservations({
+                reservations: currReservations,
+                fullYear,
+                currMonth: month + 1,
+                currDate: date
+            })}/>
+            </TimelineComponent>
+        </>
     );
 };
