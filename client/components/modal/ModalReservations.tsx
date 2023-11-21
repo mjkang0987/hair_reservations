b9a3e4d42aeafb8955da21d4adc89ab5e8cb39c7
jiwoo <snowdrop1202@gmail.com>
2023-11-21 15:42:49 +0900

    feat: 예약 전체보기 modal 내 예약별 modal 추가

--- client/components/modal/ModalReservations.tsx
+++ client/components/modal/ModalReservations.tsx
@@ -1,6 +1,15 @@
+import React, {useState} from 'react';
+
+import {createPortal} from 'react-dom';
+
 import styled from 'styled-components';
 
-import {ReservationsType} from '../../recoil/atoms';
+import {useRecoilValue} from 'recoil';
+
+import {
+    portalState,
+    ReservationsType
+} from '../../recoil/atoms';
 
 import {
     COLORS
@@ -8,35 +17,50 @@ import {
 
 import {ButtonText} from '../common/ButtonText';
 import {ButtonSquare} from '../common/Buttons';
-import React from 'react';
+import {ModalComponent} from './Modal';
+import {ModalReservation} from './ModalReservation';
+import {ModalReservationControls} from './ModalReservationControls';
 
 export const ModalReservations = ({items}: { items: ReservationsType[] }) => {
+    const portal = useRecoilValue(portalState);
+    const [isOpen, setIsOpen] = useState(false);
+    const [currItem, setCurrItem] = useState<ReservationsType | null>(null);
+
+    const handlerOpenModal = () => {
+        setIsOpen(true);
+    };
+
     return (<StyledItems>
-            {items.map((item) => <StyledItem key={`${item.name}_${item.service}`}>
-                <ButtonSquare padding={['5px']}
-                              height="auto"
-                              backgroundColor={COLORS[+item.color]}>
-                    <ButtonText a11y={false}
-                                fontSize={'var(--tiny-font)'}>{item.name} - {item.service}</ButtonText>
-                </ButtonSquare>
-            </StyledItem>)}
+            {items.map((item) => <ButtonSquare key={`${item.name}_${item.service}`}
+                                               padding={['5px']}
+                                               height="auto"
+                                               backgroundColor={COLORS[+item.color]}
+                                               onClick={() => {
+                                                   handlerOpenModal();
+                                                   setCurrItem(item);
+                                               }}>
+                <ButtonText a11y={false}
+                            fontSize={'var(--tiny-font)'}>{item.name} - {item.service}</ButtonText>
+            </ButtonSquare>)}
+
+            {(isOpen && portal && currItem) && createPortal(
+                <ModalComponent isOpen={isOpen}
+                                handlerModalClose={() => setIsOpen(false)}
+                                controls={<ModalReservationControls controls={['delete', 'modify']}/>}
+                                body={<ModalReservation item={currItem}/>}/>, portal)}
         </StyledItems>
     );
 };
 
-const StyledItems = styled.ul`
-`;
-
-const StyledItem = styled.li`
-  
-  + li {
-    margin-top: 4px;
-  }
-    
+const StyledItems = styled.div`
   button {
     justify-content: flex-start;
     width: 100%;
-    
+
+    + button {
+      margin-top: 4px;
+    }
+
     span {
       display: block;
       white-space: nowrap;
